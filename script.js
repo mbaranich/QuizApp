9ea34456fcdbbbb5516b37b1b969aa698fcfe934
Matt B <mbaranich@yahoo.com>
2021-07-25 00:48:37 -0400

    Create status classes.

--- script.js
+++ script.js
@@ -45,7 +45,26 @@ function resetState() {
 }
 
 function selectAnswer(e) {
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct);
+    })
+}
 
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct');
+    } else {
+        element.classList.add('wrong');
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
 }
 
 const questions = [
